989f9beb1abd344bf91f1cfd1fc20a79c90fd318
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-11-11 18:16:12 -0300

    login

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 //Aluno: Gabriel Koiama Rocha de Oliveira Lira - RA: 24.123.051-5
 
 
+int tipo_de_login;
+
 //Função principal que tem como objetivo chamar o menu login, onde a partir dele, todas as outras funções serão chamadas de acordo com o que o cliente pediu
 int main() {
+  menu_login();
 }

caedda09fdfca3e691426d69f1bc82f657dbb0c7
Koiama1 <114962890+Koiama1@users.noreply.github.com>
2023-11-11 18:13:10 -0300

    novo cliente e funcionário

--- funcao.c
+++ funcao.c
@@ -32,3 +32,103 @@ void salvar_funcionario(struct Funcionario funcionario) {
   fwrite(&funcionario, sizeof(struct Funcionario), 1, arquivo);
   fclose(arquivo);
 }
+//Função para ver se os clientes estão cadastrados no arquivo binario
+int cliente_existe(int cpf, int senha) {
+  FILE *arquivo;
+  struct Cliente cliente;
+
+  arquivo = fopen("clientes.dat", "rb");
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo de clientes.\n");
+    exit(1);
+  }
+
+  while (fread(&cliente, sizeof(struct Cliente), 1, arquivo)) {
+    if (cliente.cpf == cpf && cliente.senha == senha) {
+      fclose(arquivo);
+      return 1;
+    }
+  }
+
+  fclose(arquivo);
+  return 0;
+}
+
+//Função para ver se os funcionários estão cadastrados no arquivo binario
+int funcionario_existe(int cpf_funcionario, int senha) {
+  FILE *arquivo;
+  struct Funcionario funcionario;
+
+  arquivo = fopen("funcionarios.dat", "rb");
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo de funcionários.\n");
+    exit(1);
+  }
+
+  while (fread(&funcionario, sizeof(struct Funcionario), 1, arquivo)) {
+    if (funcionario.cpf_funcionario == cpf_funcionario && funcionario.senha == senha) {
+      fclose(arquivo);
+      return 1;
+    }
+  }
+
+  fclose(arquivo);
+  return 0;
+}
+
+//Função para criar um novo funcionário
+void novo_funcionario(void) {
+  int cpf_funcionario;
+  char nome[100];
+  int senha;
+
+  printf("Digite seu CPF: ");
+  scanf("%d", &cpf_funcionario);
+  printf("Digite seu nome: ");
+  scanf("%s", nome);
+  printf("Digite sua senha: ");
+  scanf("%d", &senha);
+
+  if (funcionario_existe(cpf_funcionario, senha)) {
+    printf("Funcionário já cadastrado.\n");
+  } else {
+    struct Funcionario novo_funcionario = {cpf_funcionario, "", senha};
+    strcpy(novo_funcionario.nome, nome);
+    salvar_funcionario(novo_funcionario);
+    printf("Funcionário cadastrado com sucesso!\n");
+    menu_login();
+  }
+}
+
+//Função para criar um novo cliente
+void novo_cliente(void) {
+  int cpf;
+  char nome_cliente[100];
+  char conta[10];
+  float saldo;
+  int senha;
+
+  printf("Digite seu CPF: ");
+  scanf("%d", &cpf);
+  printf("Digite seu nome: ");
+  scanf("%s", nome_cliente);
+  printf("Digite o tipo da conta (Comum ou Plus): ");
+  scanf("%s", conta);
+  printf("Digite o saldo inicial: ");
+  scanf("%f", &saldo);
+  printf("Digite sua senha: ");
+  scanf("%d", &senha);
+
+  if (cliente_existe(cpf, senha)) {
+    printf("Cliente já cadastrado.\n");
+  } else {
+    struct Cliente novo_cliente = {cpf, "", "", 0, senha};
+    strcpy(novo_cliente.nome_cliente, nome_cliente);
+    strcpy(novo_cliente.conta, conta);
+    novo_cliente.saldo = saldo;
+    salvar_cliente(novo_cliente);
+    printf("Cliente cadastrado com sucesso!\n");
+    menu_login();
+  }
+}
+
